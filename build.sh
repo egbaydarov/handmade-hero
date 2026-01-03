7d5c6193ca93dbc5ba82e9dae40cbc0f18d34938
egbaydarov <eg@byda.io>
2026-01-03 23:08:26 +0100

    chore: better build

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
+set -e
+
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+BUILD_DIR="$SCRIPT_DIR/build"
+
+rm -rf -- "$BUILD_DIR"
+mkdir -p -- "$BUILD_DIR"
+
 (
-  cd build || exit 1
-  bear --append -- $CXX ../src/main.cpp -g3 -gcodeview -o main.exe -luser32 -lgdi32
-  time "$CXX" \
-    ../src/main.cpp \
-    -g3 \
-    -gcodeview \
-    -o main.exe \
-    -l user32 \
-    -l gdi32
+  cd "$BUILD_DIR"
+
+  echo "== compile (bear, verbose) =="
+  bear --append -- \
+    "$CXX" ../src/main.cpp \
+      -ftime-report \
+      -g3 -gcodeview \
+      -o main.exe \
+      -luser32 -lgdi32
 )
 
